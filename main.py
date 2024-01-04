f7f76bf18dc0e0f7b26d60cf47b53adaac63a643
Jacob Isaacson <jisaacs46@gmail.com>
2024-01-04 10:28:51 -0700

    read book and count text

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = read_book(book_path)
+    print(text)
+    print(word_count(text))
+    print("hello world")
+
+def read_book(book_path):
+    with open(book_path) as file1:
+        text = file1.read()
+    return text
+
+def word_count(text):
+    words = text.split()
+    return len(words)
